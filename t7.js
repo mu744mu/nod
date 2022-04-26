cd7445341a8926b08cf7508aa904605abbc37372
https://ita7a.com <80185284+mu744mu@users.noreply.github.com>
2022-04-27 02:23:44 +0300

    Create t7.js

---
+++ t7.js
@@ -0,0 +1,16 @@
+		$('.hand.nosel.fl.uzr.border').css({'margin': '5px 5',borderRight: '8px solid #FFFFFF',borderLeft: '7px solid #FFFFFF',borderTop: '5px solid #FFFFFF',borderBottom: '2px solid #E9EEF7',paddingTop: '5px',borderRadius: '7px'});
+$(`<link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/mu744mu/muhanad/34ba635f10281138bb83f6a65fcd11cc708d6e42/chat.css">
+<link rel="stylesheet" href="https://cdn.statically.io/gh/mu744mu/mt/main/t7.css">  
+<link rel="stylesheet" href="">  
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 10px 10px rgba(0,0,0,.080), 0 0 10px rgb(118, 118, 22226);border-radius: }
+
+</style>`).insertBefore('body');
+   
+
+$(".fa-gear").text("  ").css("width","");
+
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 10px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #&rlm;B7B6BA;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;">
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.fr.borderg a').text('DookMobile')
